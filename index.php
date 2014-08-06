ead44f749d5f75d87f9b0e37061b05f66f7f6dca
haoqoo <qintao1203@163.com>
2014-08-06 19:39:52 +0800

    模仿wordpress主题开发

---
+++ index.php
@@ -0,0 +1,253 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<title>首页</title>
+<link href="style.css" rel="stylesheet" type="text/css" />
+</head>
+
+<body>
+<div class="top">
+  <div class="logo"><a href="index.html"><img src="images/logo.png" width="161" height="42" border="0" /></a></div>
+  <div class="qlink"><a href="#">管理员登陆</a>&nbsp;&nbsp;&nbsp;&nbsp;<a href="#">English</a></div>
+  <div class="ms">
+    <div class="nav"><a href="#">关于蓝码动力</a>&nbsp;&nbsp;&nbsp;&nbsp;<a href="#">联系我们</a></div>
+    <div class="search"><input type="text" name="textfield" id="textfield" style="width:182px; height:27px; border:0px; text-indent:5px; background:url(images/sear_10.png) no-repeat left center; line-height:27px;"/></div>
+    <div class="clear"></div>
+  </div>
+  <div class="clear"></div>
+</div>
+<div class="menu">
+    <div class="menu_m">
+     <ul class="ms_list01">
+       <li><center><a href="#">首页</a></center></li>
+       <li><center>
+         <a href="#">产品</a>
+       </center></li>
+       <li><center>
+         <a href="#">解决方案</a>
+       </center></li>
+       <li><center>
+         <a href="#">客户案例</a>
+       </center></li>
+       <li><center>
+         <a href="#">下载中心</a>
+       </center></li>
+       <li><center>
+         <a href="#">在线试用</a>
+       </center></li>
+       <li><center>
+         <a href="#">软件购买</a>
+       </center></li>
+       <li><center>
+         <a href="#">服务与支持</a>
+       </center></li>
+       <li><center>
+         <a href="#">相关文档</a>
+       </center></li>
+     </ul>
+    </div>
+</div>
+<div class="main">  
+  <div class="banner">
+     <div class="banner_m">
+      <div class="back"><a href="#"><img src="images/prev.png" width="60" height="60" border="0" /></a></div>
+	  <div class="pic">
+         <table width="100%" border="0">
+          <tr>
+            <td valign="top"><img src="images/index_22.png" width="437" height="190" /></td>
+            <td rowspan="2" align="right" valign="top"><img src="images/index_19.png" width="474" height="292" /></td>
+          </tr>
+          <tr>
+            <td valign="top"><input name="" type="button" style="background:url(images/but.png) no-repeat left center; height:38px; width:133px; color:#3fa2c7; font-size:16px; font-family:'微软雅黑'; font-style:italic; font-weight:bold; border:0px;" value="在线试用" /></td>
+           </tr>
+        </table>
+       </div>
+	  
+	  <div class="forward"><a href="#"><img src="images/next.png" width="60" height="60" border="0" /></a></div>
+     </div>
+  </div>
+  <div class="content">
+     <div class="div_allinline">
+        <div class="subdiv_allinline">
+        <table width="128" border="0">
+          <tr>
+            <td align="center"><img src="images/index_301.png" width="115" height="115" /></td>
+          </tr>
+          <tr>
+            <td align="center" height="45"><img src="images/index_45.png" width="115" height="17" /></td>
+          </tr>
+          <tr>
+            <td height="100"><p>SuperLib系列</p>
+              <p>超级图库管理软件</p>
+              <p> 网络版(B/S和C/S架构)            </p>
+            <p>单机版</p></td>
+          </tr>
+          <tr>
+            <td align="right"><input name="" type="button" style="background:#5db0d0; height:20px; width:55px; color:#ffffff; font-size:12px;border:0px;" value="更多>>" /></td>
+          </tr>
+        </table>
+       </div>
+       <div class="subdiv_allinline">
+         <table width="90" border="0"  height="115">
+          <tr>
+            <td height="115" align="center"><img src="images/arrow.png" width="72" height="18" /></td>
+          </tr>
+         </table>
+       </div>
+       <div class="subdiv_allinline">
+        <table width="128" border="0">
+          <tr>
+            <td align="center"><img src="images/index_321.png" width="115" height="115" /></td>
+          </tr>
+          <tr>
+            <td align="center" height="45"><img src="images/index_46.png" width="115" height="17" /></td>
+          </tr>
+          <tr>
+            <td height="100"><p>电力监理业务在线管理系统</p>
+              <p>设计图片管理系统</p>
+              <p> 学校图片管理系统</p>
+              <p>&nbsp;</p></td>
+          </tr>
+          <tr>
+            <td align="right"><input name="" type="button" style="background:#5db0d0; height:20px; width:55px; color:#ffffff; font-size:12px;border:0px;" value="更多>>" /></td>
+          </tr>
+        </table>
+       </div>
+       <div class="subdiv_allinline">
+         <table width="90" border="0"  height="115">
+          <tr>
+            <td height="115" align="center"><img src="images/arrow.png" width="72" height="18" /></td>
+          </tr>
+         </table>
+       </div>
+       <div class="subdiv_allinline">
+        <table width="128" border="0">
+          <tr>
+            <td align="center"><img src="images/index_341.png" width="115" height="115" /></td>
+          </tr>
+          <tr>
+            <td align="center" height="45"><img src="images/index_47.png" width="115" height="17" /></td>
+          </tr>
+          <tr>
+            <td height="100" ><p>山东送变电工程公司</p>
+              <p>第一次全国水利普查</p>
+              <p> 上海烟草集团北京卷烟厂  </p>
+            <p>上海市七宝中学</p></td>
+          </tr>
+          <tr>
+            <td align="right"><input name="" type="button" style="background:#5db0d0; height:20px; width:55px; color:#ffffff; font-size:12px;border:0px;" value="更多>>" /></td>
+          </tr>
+        </table>
+       </div>
+       <div class="subdiv_allinline">
+         <table width="90" border="0"  height="115">
+          <tr>
+            <td height="115" align="center"><img src="images/arrow.png" width="72" height="18" /></td>
+          </tr>
+         </table>
+       </div>
+       <div class="subdiv_allinline">
+        <table width="128" border="0">
+          <tr>
+            <td align="center"><img src="images/index_361.png" width="115" height="115" /></td>
+          </tr>
+          <tr>
+            <td align="center" height="45"><img src="images/index_48.png" width="115" height="17" /></td>
+          </tr>
+          <tr>
+            <td height="100"><p>超级图库管理软件3.8.72版发布</p>
+              <p>SuperLib网络版Web方式在线试用</p>
+              <p>&nbsp;</p>
+            </td>
+          </tr>
+          <tr>
+            <td align="right"><input name="" type="button" style="background:#5db0d0; height:20px; width:55px; color:#ffffff; font-size:12px;border:0px;" value="更多>>" /></td>
+          </tr>
+        </table>
+       </div>
+       <div class="subdiv_allinline">
+         <table width="90" border="0"  height="115">
+          <tr>
+            <td height="115" align="center"><img src="images/arrow.png" width="72" height="18" /></td>
+          </tr>
+         </table>
+       </div>
+       <div class="subdiv_allinline">
+        <table width="128" border="0">
+          <tr>
+            <td align="center"><img src="images/index_381.png" width="115" height="115" /></td>
+          </tr>
+          <tr>
+            <td align="center" height="45"><img src="images/index_49.png" width="115" height="17" /></td>
+          </tr>
+          <tr>
+            <td height="100"><p> 凡使用本公司产品，我们都将提供无偿地在线或来电技术支持</p>
+            <p>&nbsp;</p>
+            <p>&nbsp;</p></td>
+          </tr>
+          <tr>
+            <td align="right"><input name="" type="button" style="background:#5db0d0; height:20px; width:55px; color:#ffffff; font-size:12px;border:0px;" value="更多>>" /></td>
+          </tr>
+        </table>
+       </div>
+     </div>
+  </div>
+</div>
+<div class="foot">
+  <div class="foot_up">
+    <div class="foot_up_m">
+      <div class="foot_up_ml">您可以通过以下方式了解我们！>></div>
+      <div class="foot_up_mr">
+        <div class="div_con">
+          <div class="sub_div_con"><a href="#"><img src="images/index_62.png" width="90" height="25" border="0" /></a></div>
+          <div class="sub_div_con"><a href="#"><img src="images/index_64.png" width="106" height="25" border="0" /></a></div>
+          <div class="sub_div_con"><a href="#"><img src="images/index_66.png" width="149" height="25" border="0" /></a></div>
+          <div class="sub_div_con"><a href="#"><img src="images/index_68.png" width="135" height="25" border="0" /></a></div>
+        </div>
+      </div>
+      <div class="clear"></div>
+    </div>
+  </div>
+  <div class="foot_m">
+    <div class="foot_mm">
+      <div class="foot_mml">
+        <table width="100%" border="0">
+          <tr>
+            <td valign="top"><p><strong><span style="font-size:14px;">产品</span></strong></p>
+            <p>产品介绍</p>
+            <p>解决方案</p>
+            <p>行业应用</p></td>
+            <td valign="top"><p><strong><span style="font-size:14px;">关于</span></strong></p>
+            <p>蓝码动力介绍</p>
+            <p>加入我们</p>
+            <p>投诉建议</p>
+            <p>免责条款</p>
+            <p>公司地址</p></td>
+            <td valign="top"><p><strong><span style="font-size:14px;">售后</span></strong></p>
+            <p>售后服务政策</p>
+            <p>发票制度</p>
+            <p>退款说明</p></td>
+            <td valign="top"><p><strong><span style="font-size:14px;">友链</span></strong></p>
+            <p>手机软件</p>
+            <p>中国硅谷新闻</p>
+            <p>微信公众平台</p>
+            <p>红动原创素材</p>
+            <p>蚂蚁图库</p>
+            <p>在线设计</p>
+            <p>千图网</p></td>
+          </tr>
+        </table>
+      </div>
+      <div class="foot_mmr">
+        <p><img src="images/index_76.png" width="146" height="145" /></p>
+        <p>扫一扫加微信公众号</p>
+      </div>
+      <div class="clear"></div>
+    </div>
+  </div>
+  <div class="space2"></div>
+<div class="foot_b">京ICP备13003582 © 2006-2014 Lamma</div>
+</div>
+</body>
+</html>
