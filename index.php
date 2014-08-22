0adeeff5a8531f693a9473f4f3b8affdf1342924
haoqoo <qintao1203@163.com>
2014-08-22 17:17:46 +0800

    slide

--- index.php
+++ index.php
@@ -1,217 +1,218 @@
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml">
 <head>
-<?php
-$title       = "蓝码动力 xcoolsoft 官方网站";
-$keywords    = "蓝码动力,xcoolsoft,官方网站,图库管理软件,网络版,B/S架构,C/S架构,企业版";
-$description = "北京蓝码动力软件科技有限公司研发的超级图库管理软件，主要用于照片管理、图片管理、图纸管理、文档管理、视频管理。可统一管理、划分权限、优化、分类、搜索、浏览。专门为需要管理大量图片、文档、视频资料的单位和个人设计。";
-include ("meta.php");
-?>
-<link rel="stylesheet" type="text/css" href="js/plugins/jcarousel/jcarousel.basic.css">
+  <?php
+  $title       = "蓝码动力 xcoolsoft 官方网站";
+  $keywords    = "蓝码动力,xcoolsoft,官方网站,图库管理软件,网络版,B/S架构,C/S架构,企业版";
+  $description = "北京蓝码动力软件科技有限公司研发的超级图库管理软件，主要用于照片管理、图片管理、图纸管理、文档管理、视频管理。可统一管理、划分权限、优化、分类、搜索、浏览。专门为需要管理大量图片、文档、视频资料的单位和个人设计。";
+  include ("meta.php");
+  ?>
 
-<script type="text/javascript" src="js/plugins/jcarousel/jquery.jcarousel.min.js"></script>
-<script type="text/javascript" src="js/plugins/jcarousel/jquery.jcarousel-autoscroll.min.js"></script>
-        <script type="text/javascript" src="js/plugins/jcarousel/jcarousel.basic.js"></script>
+  <link rel="stylesheet" type="text/css" href="js/plugins/unslider/src/unslider.css" />
 
+  <script type="text/javascript" src="js/plugins/unslider/src/unslider.js"></script>
 
+  <style type="text/css">
+    .jcarousel img{
+      max-width: 474px;
+    }
+  </style>
 </head>
 
 <body>
-<?php
-require ('header.php');
-?>
-<div class="main">
-  <div class="banner">
-     <div class="banner_m">
-     <div class="pic">
-      <div class="jcarousel-wrapper">
-                <div class="jcarousel">
-                    <ul>
-                        <li>
-                          <table width="960px"  border="0" cellspacing="10">
-          <tbody><tr>
-            <td valign="top">
-            <div class="banner_title"><table><tbody><tr><td class="banner_td"><p>先试用，后购买</p></td></tr></tbody></table></div>
-            <div class="banner_text"><p>您可以在线试用，也可以把软件下载到您的电脑上安装试用。在试用时遇到问题，可随时联系我们公司的工程师解答问题。试用满意后，再签订合同，付款购买。</p>
-            </div>
-            </td>
-            <td rowspan="2" align="right" valign="top"><img src="images/pic01.png" width="474" height="249"></td>
-          </tr>
-          <tr>
-            <td valign="top"><input name="" type="button" style="background:url(images/but.png) no-repeat left center; height:38px; width:133px; color:#3fa2c7; font-size:16px; font-family:'微软雅黑'; font-style:italic; font-weight:bold; border:0px;" value="在线试用">&nbsp;
-&nbsp;
-<input name="" type="button" style="background:url(images/but.png) no-repeat left center; height:38px; width:133px; color:#3fa2c7; font-size:16px; font-family:'微软雅黑'; font-style:italic; font-weight:bold; border:0px;" value="下载试用">
-            </td>
-           </tr>
-        </tbody></table>
-                        </li>
+  <?php
+  require ('header.php');
+  ?>
+  <div class="main">
+    <div class="banner">
+     <div class="banner_m" style="padding:65px 0px;">          
+        <div class="jcarousel">
+          <ul>
+            <li>
+              <table width=""  border="0" cellspacing="10">
+                <tbody><tr>
+                  <td valign="top" width="620px">
+                    <div class="banner_title"><table><tbody><tr><td class="banner_td"><p>先试用，后购买</p></td></tr></tbody></table></div>
+                    <div class="banner_text"><p>您可以在线试用，也可以把软件下载到您的电脑上安装试用。在试用时遇到问题，可随时联系我们公司的工程师解答问题。
+                    试用满意后，再签订合同，付款购买。</p>
+                    </div>
+                  </td>
+                  <td rowspan="2"  valign="top"><img src="images/pic01.png"></td>
+                </tr>
+                <tr>
+                  <td valign="top" width="620px"><input name="" type="button" style="background:url(images/but.png) no-repeat left center; height:38px; width:133px; color:#3fa2c7; font-size:16px; font-family:'微软雅黑'; font-style:italic; font-weight:bold; border:0px;" value="在线试用">&nbsp;
+                    &nbsp;
+                    <input name="" type="button" style="background:url(images/but.png) no-repeat left center; height:38px; width:133px; color:#3fa2c7; font-size:16px; font-family:'微软雅黑'; font-style:italic; font-weight:bold; border:0px;" value="下载试用">
+                  </td>
+                </tr>
+              </tbody></table>
+            </li>
 
-                        <li>
-                          <table width="960px" border="0" cellspacing="10">
-          <tbody><tr>
-            <td valign="top">
-            <div class="banner_title"><table><tbody><tr><td class="banner_td"><p>先试用，后购买</p></td></tr></tbody></table></div>
-            <div class="banner_text"><p>您可以在线试用，也可以把软件下载到您的电脑上安装试用。在试用时遇到问题，可随时联系我们公司的工程师解答问题。试用满意后，再签订合同，付款购买。</p>
-            </div>
-            </td>
-            <td rowspan="2" align="right" valign="top"><img src="images/pic02.png" width="474" height="249"></td>
-          </tr>
-          <tr>
-            <td valign="top"><input name="" type="button" style="background:url(images/but.png) no-repeat left center; height:38px; width:133px; color:#3fa2c7; font-size:16px; font-family:'微软雅黑'; font-style:italic; font-weight:bold; border:0px;" value="在线试用">&nbsp;
-&nbsp;
-<input name="" type="button" style="background:url(images/but.png) no-repeat left center; height:38px; width:133px; color:#3fa2c7; font-size:16px; font-family:'微软雅黑'; font-style:italic; font-weight:bold; border:0px;" value="下载试用">
-            </td>
-           </tr>
-        </tbody></table>
-                        </li>
-
-                    </ul>
-                </div>
-
-
-
-                <a href="#" class="jcarousel-control-prev"><img src="images/prev.png" width="60" height="60" border="0"></a>
-                <a href="#" class="jcarousel-control-next"><img src="images/next.png" width="60" height="60" border="0" /></a>
-
-
-            </div>
-            </div>
-     </div>
+            <li>
+              <table width="" border="0" cellspacing="10">
+                <tbody><tr>
+                  <td valign="top" >
+                    <div class="banner_title"><table><tbody><tr><td class="banner_td"><p>先试用，后购买</p></td></tr></tbody></table></div>
+                    <div class="banner_text"><p>您可以在线试用，也可以把软件下载到您的电脑上安装试用。在试用时遇到问题，可随时联系我们公司的工程师解答问题。试用满意后，再签订合同，付款购买。</p>
+                    </div>
+                  </td>
+                  <td rowspan="2"  valign="top"><img src="images/pic02.png" ></td>
+                </tr>
+                <tr>
+                  <td valign="top"><input name="" type="button" style="background:url(images/but.png) no-repeat left center; height:38px; width:133px; color:#3fa2c7; font-size:16px; font-family:'微软雅黑'; font-style:italic; font-weight:bold; border:0px;" value="在线试用">&nbsp;
+                    &nbsp;
+                    <input name="" type="button" style="background:url(images/but.png) no-repeat left center; height:38px; width:133px; color:#3fa2c7; font-size:16px; font-family:'微软雅黑'; font-style:italic; font-weight:bold; border:0px;" value="下载试用">
+                  </td>
+                </tr>
+              </tbody></table>
+            </li>
+          </ul>
+          
+        </div>
+    
+    </div>
   </div>
   <div class="content">
-     <div class="div_allinline">
-        <div class="subdiv_allinline">
-        <table width="128" border="0">
-          <tr>
-            <td align="center"><img src="images/index_301.png" width="115" height="115" /></td>
-          </tr>
-          <tr>
-            <td align="center" height="45"><img src="images/index_45.png" width="115" height="17" /></td>
-          </tr>
-          <tr>
-            <td height="100"><p>SuperLib系列</p>
-              <p>超级图库管理软件</p>
-              <p> 网络版(B/S和C/S架构)            </p>
+   <div class="div_allinline">
+    <div class="subdiv_allinline">
+      <table width="128" border="0">
+        <tr>
+          <td align="center"><img src="images/index_301.png" width="115" height="115" /></td>
+        </tr>
+        <tr>
+          <td align="center" height="45"><img src="images/index_45.png" width="115" height="17" /></td>
+        </tr>
+        <tr>
+          <td height="100"><p>SuperLib系列</p>
+            <p>超级图库管理软件</p>
+            <p> 网络版(B/S和C/S架构)            </p>
             <p>单机版</p></td>
           </tr>
           <tr>
             <td align="right"><input name="" type="button" style="background:#5db0d0; height:20px; width:55px; color:#ffffff; font-size:12px;border:0px;" value="更多>>" /></td>
           </tr>
         </table>
-       </div>
-       <div class="subdiv_allinline">
-         <table width="90" border="0"  height="115">
-          <tr>
-            <td height="115" align="center"><img src="images/arrow.png" width="72" height="18" /></td>
-          </tr>
-         </table>
-       </div>
-       <div class="subdiv_allinline">
-        <table width="128" border="0">
-          <tr>
-            <td align="center"><img src="images/index_321.png" width="115" height="115" /></td>
-          </tr>
-          <tr>
-            <td align="center" height="45"><img src="images/index_46.png" width="115" height="17" /></td>
-          </tr>
-          <tr>
-            <td height="100"><p>电力监理业务在线管理系统</p>
-              <p>设计图片管理系统</p>
-              <p> 学校图片管理系统</p>
-              <p>&nbsp;</p></td>
-          </tr>
-          <tr>
-            <td align="right"><input name="" type="button" style="background:#5db0d0; height:20px; width:55px; color:#ffffff; font-size:12px;border:0px;" value="更多>>" /></td>
-          </tr>
-        </table>
-       </div>
-       <div class="subdiv_allinline">
-         <table width="90" border="0"  height="115">
-          <tr>
-            <td height="115" align="center"><img src="images/arrow.png" width="72" height="18" /></td>
-          </tr>
-         </table>
-       </div>
-       <div class="subdiv_allinline">
-        <table width="128" border="0">
-          <tr>
-            <td align="center"><img src="images/index_341.png" width="115" height="115" /></td>
-          </tr>
-          <tr>
-            <td align="center" height="45"><img src="images/index_47.png" width="115" height="17" /></td>
-          </tr>
-          <tr>
-            <td height="100" ><p>山东送变电工程公司</p>
-              <p>第一次全国水利普查</p>
-              <p> 上海烟草集团北京卷烟厂  </p>
-            <p>上海市七宝中学</p></td>
-          </tr>
-          <tr>
-            <td align="right"><input name="" type="button" style="background:#5db0d0; height:20px; width:55px; color:#ffffff; font-size:12px;border:0px;" value="更多>>" /></td>
-          </tr>
-        </table>
-       </div>
-       <div class="subdiv_allinline">
-         <table width="90" border="0"  height="115">
-          <tr>
-            <td height="115" align="center"><img src="images/arrow.png" width="72" height="18" /></td>
-          </tr>
-         </table>
-       </div>
-       <div class="subdiv_allinline">
-        <table width="128" border="0">
-          <tr>
-            <td align="center"><img src="images/index_361.png" width="115" height="115" /></td>
-          </tr>
-          <tr>
-            <td align="center" height="45"><img src="images/index_48.png" width="115" height="17" /></td>
-          </tr>
-          <tr>
-            <td height="100"><p>超级图库管理软件3.8.72版发布</p>
-              <p>SuperLib网络版Web方式在线试用</p>
-              <p>&nbsp;</p>
-            </td>
-          </tr>
-          <tr>
-            <td align="right"><input name="" type="button" style="background:#5db0d0; height:20px; width:55px; color:#ffffff; font-size:12px;border:0px;" value="更多>>" /></td>
-          </tr>
-        </table>
-       </div>
-       <div class="subdiv_allinline">
-         <table width="90" border="0"  height="115">
-          <tr>
-            <td height="115" align="center"><img src="images/arrow.png" width="72" height="18" /></td>
-          </tr>
-         </table>
-       </div>
-       <div class="subdiv_allinline">
-        <table width="128" border="0">
-          <tr>
-            <td align="center"><img src="images/index_381.png" width="115" height="115" /></td>
-          </tr>
-          <tr>
-            <td align="center" height="45"><img src="images/index_49.png" width="115" height="17" /></td>
-          </tr>
-          <tr>
-            <td height="100"><p> 凡使用本公司产品，我们都将提供无偿地在线或来电技术支持</p>
-            <p>&nbsp;</p>
+      </div>
+      <div class="subdiv_allinline">
+       <table width="90" border="0"  height="115">
+        <tr>
+          <td height="115" align="center"><img src="images/arrow.png" width="72" height="18" /></td>
+        </tr>
+      </table>
+    </div>
+    <div class="subdiv_allinline">
+      <table width="128" border="0">
+        <tr>
+          <td align="center"><img src="images/index_321.png" width="115" height="115" /></td>
+        </tr>
+        <tr>
+          <td align="center" height="45"><img src="images/index_46.png" width="115" height="17" /></td>
+        </tr>
+        <tr>
+          <td height="100"><p>电力监理业务在线管理系统</p>
+            <p>设计图片管理系统</p>
+            <p> 学校图片管理系统</p>
             <p>&nbsp;</p></td>
           </tr>
           <tr>
             <td align="right"><input name="" type="button" style="background:#5db0d0; height:20px; width:55px; color:#ffffff; font-size:12px;border:0px;" value="更多>>" /></td>
           </tr>
         </table>
-       </div>
-     </div>
+      </div>
+      <div class="subdiv_allinline">
+       <table width="90" border="0"  height="115">
+        <tr>
+          <td height="115" align="center"><img src="images/arrow.png" width="72" height="18" /></td>
+        </tr>
+      </table>
+    </div>
+    <div class="subdiv_allinline">
+      <table width="128" border="0">
+        <tr>
+          <td align="center"><img src="images/index_341.png" width="115" height="115" /></td>
+        </tr>
+        <tr>
+          <td align="center" height="45"><img src="images/index_47.png" width="115" height="17" /></td>
+        </tr>
+        <tr>
+          <td height="100" ><p>山东送变电工程公司</p>
+            <p>第一次全国水利普查</p>
+            <p> 上海烟草集团北京卷烟厂  </p>
+            <p>上海市七宝中学</p></td>
+          </tr>
+          <tr>
+            <td align="right"><input name="" type="button" style="background:#5db0d0; height:20px; width:55px; color:#ffffff; font-size:12px;border:0px;" value="更多>>" /></td>
+          </tr>
+        </table>
+      </div>
+      <div class="subdiv_allinline">
+       <table width="90" border="0"  height="115">
+        <tr>
+          <td height="115" align="center"><img src="images/arrow.png" width="72" height="18" /></td>
+        </tr>
+      </table>
+    </div>
+    <div class="subdiv_allinline">
+      <table width="128" border="0">
+        <tr>
+          <td align="center"><img src="images/index_361.png" width="115" height="115" /></td>
+        </tr>
+        <tr>
+          <td align="center" height="45"><img src="images/index_48.png" width="115" height="17" /></td>
+        </tr>
+        <tr>
+          <td height="100"><p>超级图库管理软件3.8.72版发布</p>
+            <p>SuperLib网络版Web方式在线试用</p>
+            <p>&nbsp;</p>
+          </td>
+        </tr>
+        <tr>
+          <td align="right"><input name="" type="button" style="background:#5db0d0; height:20px; width:55px; color:#ffffff; font-size:12px;border:0px;" value="更多>>" /></td>
+        </tr>
+      </table>
+    </div>
+    <div class="subdiv_allinline">
+     <table width="90" border="0"  height="115">
+      <tr>
+        <td height="115" align="center"><img src="images/arrow.png" width="72" height="18" /></td>
+      </tr>
+    </table>
   </div>
+  <div class="subdiv_allinline">
+    <table width="128" border="0">
+      <tr>
+        <td align="center"><img src="images/index_381.png" width="115" height="115" /></td>
+      </tr>
+      <tr>
+        <td align="center" height="45"><img src="images/index_49.png" width="115" height="17" /></td>
+      </tr>
+      <tr>
+        <td height="100"><p> 凡使用本公司产品，我们都将提供无偿地在线或来电技术支持</p>
+          <p>&nbsp;</p>
+          <p>&nbsp;</p></td>
+        </tr>
+        <tr>
+          <td align="right"><input name="" type="button" style="background:#5db0d0; height:20px; width:55px; color:#ffffff; font-size:12px;border:0px;" value="更多>>" /></td>
+        </tr>
+      </table>
+    </div>
+  </div>
+</div>
 </div>
 <?php
 require ('footer.php');
 ?>
 <script type="text/javascript">
   jQuery(document).ready(function() {
-
-    });
+     $('.jcarousel').unslider({
+        speed: 500,               //  The speed to animate each slide (in milliseconds)
+        delay: 3000,              //  The delay between slide animations (in milliseconds), false for no autoplay
+        complete: function() {},  //  A function that gets called after every slide animation
+        keys: true,               //  Enable keyboard (left, right) arrow shortcuts
+        dots: true,               //  Display dot navigation
+        fluid: false              //  Support responsive design. May break non-responsive designs
+      });
+  });
 </script>
 </body>
 </html>
